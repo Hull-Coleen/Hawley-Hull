1897bf0837383e5f178643038a75f8d12dd8d934
Coleen <Coleen@192.168.253.1>
2016-01-28 19:10:45 -0500

    Test Coleen

---
+++ Pirates/src/pirates/Pirates.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pirates;
+
+import byui.cit260.pirates.model.Player;
+
+/**
+ *
+ * @author Coleen
+ */
+public class Pirates {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Player playerOne = new Player();
+         //playerOne.setName("Coleen");
+         //String
+         
+    }
