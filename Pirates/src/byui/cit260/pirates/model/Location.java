4e1439392605942a4b4673ab63ce8c947a4f768b
Coleen <Coleen@Coleen-PC>
2016-01-30 13:39:42 -0500

    Test Coleen

---
+++ Pirates/src/byui/cit260/pirates/model/Location.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pirates.model;
+
+/**
+ *
+ * @author Thomas
+ */
+public class Location {
+    int row;
+    int col;
+
+    public Location(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public Location() {
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + this.row;
+        hash = 83 * hash + this.col;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (this.row != other.row) {
+            return false;
+        }
+        if (this.col != other.col) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "row=" + row + ", col=" + col + '}';
+    }
+    
+    
+}
