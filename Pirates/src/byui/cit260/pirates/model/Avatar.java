97e47b2b08264d1dfd64bd76e67bd4980eeb9d0f
Coleen <Coleen@Coleen-PC>
2016-01-30 13:39:58 -0500

    Test Coleen

---
+++ Pirates/src/byui/cit260/pirates/model/Avatar.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pirates.model;
+
+import java.util.Objects;
+//import byui.cit260.pirates.model.Location;
+/**
+ *
+ * @author Thomas
+ */
+public class Avatar {
+    boolean pirate;
+    Location coord;
+    String name;
+    public Avatar() {
+    }
+
+    public Avatar(boolean pirate, Location coord, String name) {
+        this.pirate = pirate;
+        this.coord = coord;
+        this.name = name;
+    }
+
+    public boolean isPirate() {
+        return pirate;
+    }
+
+    public void setPirate(boolean pirate) {
+        this.pirate = pirate;
+    }
+
+    public Location getCoord() {
+        return coord;
+    }
+
+    public void setCoord(Location coord) {
+        this.coord = coord;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + (this.pirate ? 1 : 0);
+        hash = 37 * hash + Objects.hashCode(this.coord);
+        hash = 37 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Avatar other = (Avatar) obj;
+        if (this.pirate != other.pirate) {
+            return false;
+        }
+        if (!Objects.equals(this.coord, other.coord)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Avatar{" + "pirate=" + pirate + ", coord=" + coord + ", name=" + name + '}';
+    }
+    
+
+    
+
+   
+    
+}
