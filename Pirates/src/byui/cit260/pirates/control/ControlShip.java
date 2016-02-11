014531b14a913db0d521a7615fe2cb6f8db567db
Coleen <Coleen@Coleen-PC>
2016-02-11 13:14:38 -0500

    Test Coleen

---
+++ Pirates/src/byui/cit260/pirates/control/ControlShip.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pirates.control;
+
+/**
+ *
+ * @author Coleen
+ */
+public class ControlShip {
+    public void moveShip(){}
+    public void fireCannon(){}
+    public int checkAmmo(){
+        int ammo = 0;
+    return ammo;
+    }
+    
+}
