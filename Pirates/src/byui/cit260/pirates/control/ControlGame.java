7d154a6e7235d99937603a5fd6a9b49a7a6813c6
Coleen <Coleen@Coleen-PC>
2016-02-11 13:14:27 -0500

    Test Coleen

---
+++ Pirates/src/byui/cit260/pirates/control/ControlGame.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pirates.control;
+
+import byui.cit260.pirates.model.Player;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Coleen
+ */
+public class ControlGame implements Serializable{
+    public void startGame(){}
+    public void startMap(){}
+    public void saveGame(Player player){}
+    public void readGame(Player player){}
+    public void newPlayer(){}
+    
+}
