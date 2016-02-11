d46e3c33c769074abe0949f2b57afaf8c04f0f5f
Coleen <Coleen@Coleen-PC>
2016-02-11 13:20:18 -0500

    Test Coleen

---
+++ Pirates/src/byui/cit260/pirates/control/ControlBattles.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pirates.control;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Coleen
+ */
+public class ControlBattles implements Serializable{
+    public int battleStrength(){
+    int strength = 0;
+    return strength;
+    }
+    public int battleSpeed(){
+        int speed = 0;
+        return speed;
+        
+    }
+    
+    
+}
