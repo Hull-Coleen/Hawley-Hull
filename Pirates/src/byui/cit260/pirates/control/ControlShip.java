431e6fa17d090f3511e6df0d480bbde3bed04826
Coleen <Coleen@192.168.253.1>
2016-02-12 09:18:26 -0500

    Test Coleen

--- Pirates/src/byui/cit260/pirates/control/ControlShip.java
+++ Pirates/src/byui/cit260/pirates/control/ControlShip.java
@@ -10,6 +10,17 @@ package byui.cit260.pirates.control;
  * @author Coleen
  */
 public class ControlShip {
+    public int daysOfFood(int numFoodCrate, int numCrew)
+    {
+        if (numFoodCrate < 0 || numCrew <= 0)
+              return -1;
+        if (numFoodCrate >= 50 || numCrew >= 9)
+              return -1;
+        int numOfMeals = numFoodCrate * 9;
+        int daysOfFood = (numOfMeals / numCrew) / 3;
+        return daysOfFood;
+    }
+
     public void moveShip(){}
     public void fireCannon(){}
     public int checkAmmo(){
