5907fc3eaab430116dfba73985e044c6b4492957
Coleen <Coleen@192.168.253.1>
2016-03-03 22:18:54 -0500

    Test Coleen

--- Pirates/src/byui/cit260/pirates/view/MoveView.java
+++ Pirates/src/byui/cit260/pirates/view/MoveView.java
@@ -6,42 +6,30 @@
 package byui.cit260.pirates.view;
 
 import java.io.Serializable;
-import java.util.Scanner;
+
 
 /**
  *
  * @author hawle_000
  */
-public class MoveView implements Serializable{
-     private final String MOVEMENU = "\n"          
-            + "\n-------------------------------"
-            + "\n       Game Start Menu         "
+public class MoveView extends View implements Serializable{
+
+    public MoveView() {
+        super("\n-------------------------------"
+            + "\n       Move  Menu              "
             + "\n-------------------------------"
             + "\nW - Move up"
             + "\nS - Move down"
             + "\nA - Move left"
             + "\nD - Move right"
             + "\nE - Exit"
-            + "\n--------------------------------";
-    private String getInput() {
-        boolean valid = false;
-        String input = null;
-        Scanner keyboard = new Scanner(System.in);
-    
-        while(!valid){
-            input = keyboard.nextLine().toUpperCase();
-           
-            if (input.length() < 1){
-            System.out.println("Invalid name: must not be empty");
-            continue;
-            }
-            //break;
-            valid = true;
-        }
-      
-        return input;
+            + "\n--------------------------------");
     }
-    private void doAction(char selection) {
+    @Override
+    public boolean doAction(String value) {
+         value = value.toUpperCase();
+    char selection;
+     selection = value.charAt(0); 
        switch(selection){
            case 'W': 
                this.moveShipUp();
@@ -56,22 +44,14 @@ public class MoveView implements Serializable{
                this.moveShipRight();
                break;
            case 'E': 
-               return;
+               return true;
            default:
                System.out.println("Invalid Entry");
                break;
        }
+       return false;
     }
-    void displayMenu() {
-        
-         char selection = ' ' ;
-       do {
-           System.out.println(MOVEMENU);
-           String input = this.getInput().toUpperCase();
-           selection = input.charAt(0);
-           this.doAction(selection);        
-       }while(selection != 'E');
-    }
+  
     private void moveShipUp() {
         System.out.println("Move ship up stubbed");
         
